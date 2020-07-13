b4ef7ab4c8aca1a7d487d69a06a910dd05d60d31
Axurj <acerj2468@gmail.com>
2020-07-13 15:41:48 -0700

    Added comments

--- form.js
+++ form.js
@@ -1,7 +1,11 @@
+// Gets the name from the answer input tag
 let nameRecieved = document.getElementById('answer');
+// When you type letters into the form box, it will activate the getName function.
 nameRecieved.addEventListener('keyup', getName);
+// Gets p tag with the id of print.
 let printer = document.getElementById('print');
 
+// Takes the answer from the input element in the form and puts it into a p tag.
 function getName() {
     printer.innerHTML = nameRecieved.value;
 }
